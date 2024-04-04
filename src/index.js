2f758ce496d2748e3511945c95857761f779aa98
arpit5492 <arpitmallick99@gmail.com>
2024-04-04 02:25:54 -0700

    Changes updated

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { Provider } from 'react-redux';
-// import { configureStore } from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
 import loginReducer from './store/loginSlice';
 import prodReducer from './store/prodSlice';
 
